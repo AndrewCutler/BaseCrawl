b87fa7ea74f349fcba9e5886e795c67ec2be3bf3
Andrew Cutler <Andrew.Cutler@fusionmgt.com>
2022-07-03 02:05:02 -0400

    build player lookup without writing to csv

--- src/crawl.ts
+++ src/crawl.ts
@@ -1,4 +1,4 @@
-import { PlayerStats, IStats, STATS, Constants } from './models';
+import { Constants, IStats, PlayerStats, STATS } from './models';
 
 const cheerio = require('cheerio');
 const express = require('express');
@@ -7,7 +7,7 @@ const cors = require('cors');
 const fs = require('fs');
 
 const corsOptions = {
-    origin: process.env.APP_ORIGIN || 'http://localhost:3000'
+	origin: process.env.APP_ORIGIN || 'http://localhost:3000'
 };
 const PORT = process.env.PORT || '1986';
 const baseUrl = Constants.BASEURL;
@@ -15,116 +15,113 @@ const playerData = JSON.parse(fs.readFileSync(Constants.DATA_JSON, { encoding: '
 
 const server = express(/*cors()*/);
 
-const buildPlayerLookup = () => {
-    fs.readFile(Constants.DATA_CSV, (err, data) => {
-        if (err) throw err;
-        const playerArray = data.toString().split('\n').map(p => p.split(','));
+const buildPlayerLookup = (data) => {
+	const playerArray = data.toString().split('\n').map(p => p.split(','));
 
-        const playerLookup = playerArray.reduce((prev, curr) => {
-            const [endpoint, name, years] = curr;
-            const entry = {
-                Endpoint: endpoint,
-                Name: name,
-                Years: years
-            };
+	const playerLookup = playerArray.reduce((prev, curr) => {
+		const [endpoint, name, years] = curr;
+		const entry = {
+			Endpoint: endpoint,
+			Name: name,
+			Years: years
+		};
 
-            if (prev[name]) {
-                prev[name] = [...prev[name], entry]
-            } else {
-                prev[name] = [entry];
-            }
+		if (prev[name]) {
+			prev[name] = [...prev[name], entry]
+		} else {
+			prev[name] = [entry];
+		}
 
-            return prev;
-        }, {});
+		return prev;
+	}, {});
 
-        fs.writeFile('players.json', JSON.stringify(playerLookup), console.error)
-    });
+	fs.writeFile('players.json', JSON.stringify(playerLookup), console.error)
 }
 
 const stripStyling = (html: string): string => html.replace(/(<\/?strong>)|(<\/?em>)/g, '');
 
 // This content is commented out on page load. Not sure of a more elegant soluation.
 const uncommentValueTable = (data: string): string => {
-    return data
-        .replace('<!--\n\n<div class="table_container" id="div_batting_value">', '<div class="table_container" id="div_batting_value">')
-        .replace('</table>\n\n\n</div>\n-->', '</table>\n\n\n</div>\n');
+	return data
+		.replace('<!--\n\n<div class="table_container" id="div_batting_value">', '<div class="table_container" id="div_batting_value">')
+		.replace('</table>\n\n\n</div>\n-->', '</table>\n\n\n</div>\n');
 }
 
 // buildPlayerLookup();
 
 const getPlayerStats = (url: string) => {
-    return axios.get(url).then((response) => {
-        if (response && response.data) {
-            const $ = cheerio.load(uncommentValueTable(response.data));
+	return axios.get(url).then((response) => {
+		if (response && response.data) {
+			const $ = cheerio.load(uncommentValueTable(response.data));
 
-            const getStandardBattingYears = (): any[] =>
-                $('tr[id^="batting_standard."]').each((_, element) => $(element).html());
-            const getStatByStandardBattingYear = (stat: string, year: string) => {
-                const result = $(`[id="${year}"] [data-stat="${stat}"]`).html()
+			const getStandardBattingYears = (): any[] =>
+				$('tr[id^="batting_standard."]').each((_, element) => $(element).html());
+			const getStatByStandardBattingYear = (stat: string, year: string) => {
+				const result = $(`[id="${year}"] [data-stat="${stat}"]`).html()
 
-                return stripStyling(result);
-            }
-            const getStatByCareer = (stat: string) => {
-                const result = $(`#all_batting_standard tfoot tr td[data-stat=${stat}]`).html();
+				return stripStyling(result);
+			}
+			const getStatByCareer = (stat: string) => {
+				const result = $(`#all_batting_standard tfoot tr td[data-stat=${stat}]`).html();
 
-                return stripStyling(result);
-            }
-            const getWAR = (year) => $(`tr[id="batting_value.${year}"] [data-stat="WAR"]`);
+				return stripStyling(result);
+			}
+			const getWAR = (year) => $(`tr[id="batting_value.${year}"] [data-stat="WAR"]`);
 
-            let playerStats: PlayerStats = new PlayerStats();
+			let playerStats: PlayerStats = new PlayerStats();
 
-            for (const row of getStandardBattingYears()) {
-                const id = row.attribs.id;
-                const year = id.split('.')[1];
-                const playerAge = getStatByStandardBattingYear('age', id);
+			for (const row of getStandardBattingYears()) {
+				const id = row.attribs.id;
+				const year = id.split('.')[1];
+				const playerAge = getStatByStandardBattingYear('age', id);
 
-                let stats: IStats;
-                STATS.forEach(stat => {
-                    const value = getStatByStandardBattingYear(stat, id);
-                    stats = {
-                        ...stats,
-                        [stat]: parseFloat(value),
-                        Year: year,
-                        Age: playerAge
-                    }
-                });
+				let stats: IStats;
+				STATS.forEach(stat => {
+					const value = getStatByStandardBattingYear(stat, id);
+					stats = {
+						...stats,
+						[stat]: parseFloat(value),
+						Year: year,
+						Age: playerAge
+					}
+				});
 
-                stats = {
-                    ...stats,
-                    'WAR': +getWAR(year).text()
-                }
+				stats = {
+					...stats,
+					'WAR': +getWAR(year).text()
+				}
 
-                playerStats = {
-                    ...playerStats,
-                    Ages: { ...playerStats.Ages, [playerAge]: stats },
-                    Years: { ...playerStats.Years, [year]: stats },
-                };
-            }
+				playerStats = {
+					...playerStats,
+					Ages: { ...playerStats.Ages, [playerAge]: stats },
+					Years: { ...playerStats.Years, [year]: stats },
+				};
+			}
 
-            let careerStats: IStats;
-            STATS.forEach(stat => {
-                const value = getStatByCareer(stat);
-                careerStats = {
-                    ...careerStats,
-                    [stat]: parseFloat(value),
-                    Year: 'Career',
-                    Age: 'Career'
-                }
-            });
+			let careerStats: IStats;
+			STATS.forEach(stat => {
+				const value = getStatByCareer(stat);
+				careerStats = {
+					...careerStats,
+					[stat]: parseFloat(value),
+					Year: 'Career',
+					Age: 'Career'
+				}
+			});
 
-            // TODO: grab career WAR
+			// TODO: grab career WAR
 
-            playerStats = {
-                ...playerStats,
-                Ages: { ...playerStats.Ages, Career: careerStats },
-                Years: { ...playerStats.Years, Career: careerStats },
-            }
+			playerStats = {
+				...playerStats,
+				Ages: { ...playerStats.Ages, Career: careerStats },
+				Years: { ...playerStats.Years, Career: careerStats },
+			}
 
-            return playerStats;
-        } else {
-            console.error(`Missing response for ${url}`);
-        }
-    });
+			return playerStats;
+		} else {
+			console.error(`Missing response for ${url}`);
+		}
+	});
 };
 
 /**
@@ -135,7 +132,7 @@ const getPlayerStats = (url: string) => {
  * Useless landing route.
  */
 server.get('/', (req, res) => {
-    res.send('Home plate.');
+	res.send('Home plate.');
 });
 
 /**
@@ -143,46 +140,45 @@ server.get('/', (req, res) => {
  * @returns Lookup of name to list of players with that name.
  */
 server.get('/search/:name', cors(corsOptions), async (req, res, next) => {
-    const { params: { name } } = req;
+	const { params: { name } } = req;
 
-    try {
-        const matches = new Set<string>();
-        const _name = name.toLowerCase();
-        for (const key in playerData) {
-            if (key.toLowerCase().includes(_name) && !matches.has(_name)) {
-                matches.add(key);
-            }
-        }
+	try {
+		const matches = new Set<string>();
+		const _name = name.toLowerCase();
+		for (const key in playerData) {
+			if (key.toLowerCase().includes(_name) && !matches.has(_name)) {
+				matches.add(key);
+			}
+		}
 
-        const result = Array.from(matches)
-            .reduce((prev, key) => ({ ...prev, [key]: playerData[key] }), {});
+		const result = Array.from(matches)
+			.reduce((prev, key) => ({ ...prev, [key]: playerData[key] }), {});
 
-        res.json(result);
-    } catch (error) {
-        next(error);
-    }
+		res.json(result);
+	} catch (error) {
+		next(error);
+	}
 });
 
 /**
  * Grabs all stats for a given player organized into year.
  */
 server.get('/stats/:endpoint', cors(corsOptions), async ({ params: { endpoint } }, res, next) => {
-    const playerUrl = `${baseUrl}/players/${endpoint[0]}/${endpoint}.shtml`;
+	const playerUrl = `${baseUrl}/players/${endpoint[0]}/${endpoint}.shtml`;
 
-    try {
-        const result = await getPlayerStats(playerUrl);
+	try {
+		const result = await getPlayerStats(playerUrl);
 
-        res.json(result);
-    } catch (error) {
-        next(error)
-    }
+		res.json(result);
+	} catch (error) {
+		next(error)
+	}
 });
 
 server.get('/refresh', (req, res) => {
-    axios.get(Constants.DATA_CSV_URL).then((response) => {
-        fs.writeFile(Constants.DATA_CSV, response.data, console.error);
-        buildPlayerLookup();
-    });
+	axios.get(Constants.DATA_CSV_URL).then((response) => {
+		buildPlayerLookup(response.data);
+	});
 })
 
 server.listen(PORT, () => console.log(`Started sever on port ${PORT}.`));
